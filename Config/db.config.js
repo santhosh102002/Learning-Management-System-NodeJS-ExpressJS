fce8da1bde805044217ea62b698286ee67a47afb
KADIYAM SANTHOSH KUMAR <santhosh102002@gmail.com>
2023-08-14 16:30:58 +0530

    AppError File created in Utils

--- Config/db.config.js
+++ Config/db.config.js
@@ -1,15 +1,29 @@
 const mongoose = require("mongoose");
 
+
 mongoose.set('strictQuery',false) // Dont throw error if any field not found in DB
 
-const dbconnect = ()=>{
-    mongoose.connect(process.env.MONGO_URL)
-    .then((conn)=>{
-        console.log(`Connected successfully to MongoDB ${conn.connection.host}`);
-    })
-    .catch((err)=>{
-        console.log(`Failed to connect to MongoDB`)
-    })
+// const dbconnect = ()=>{
+//     mongoose.connect(process.env.MONGO_URL)
+//     .then((conn)=>{
+//         console.log(`Connected successfully to MongoDB ${conn.connection.host}`);
+//     })
+//     .catch((err)=>{
+//         console.log(`Failed to connect to MongoDB`)
+//     })
+// }
+
+const dbconnect = async ()=>{
+    try{
+        const {connection} = await mongoose.connect(process.env.MONGO_URI);
+        if(connection){
+        console.log(`Successfully connected to DB ${connection.host}`)
+        }
+    }
+    catch(err){
+        console.log('Failed to connect to DB')
+        process.exit(1)
+    }
 }
 
 module.exports = dbconnect
--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -1,8 +1,32 @@
+const User = require("../models/user.models");
+const { default: AppError } = require("../utils/appError");
 
 
 
-const register = ()=>{
+const register = async (req,res)=>{
+ const {fullname,email,password} = req.body;
+ if(!fullname || !email || !password){
+    return next(new AppError("All fields are required",400))
+ }
+ const UserExists = await User.findOne({email})
+ if(UserExists){
+    return next(new AppError("User Already exists ",400))
+}
+const userCreate = await User.create({
+    fullname,
+    email,
+    password
+}
+)
 
+if(!userCreate){
+    return next(new AppError("Registering user if failed, Try again",400))
+}
+await userCreate.save();
+res.status(200).json({
+    success: true,
+    message: "User registered successfully"
+})
 }
 
 const login = ()=>{
